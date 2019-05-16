194acd1f257d5728bd79515b890470efd57c2f8a
Wouterr0 <ruiterwouter@gmail.com>
2019-05-16 19:43:36 +0200

    Make p and q random primes insted of jusing hardcoded values.

--- rsa.py
+++ rsa.py
@@ -1,5 +1,7 @@
 import numpy as np
 from random import randint
+from Crypto.Util import number
+
 
 def egcd(b, n):
     (x0, x1, y0, y1) = (1, 0, 0, 1)
@@ -18,7 +20,7 @@ def modinv(a, m):
         return x % m
 
 def strtonum(msg):
-	return (int.from_bytes(msg.encode(), "big"), len(msg))
+    return (int.from_bytes(msg.encode(), "big"), len(msg))
 def numtostr(num, length):
     return int.to_bytes(num, length, "big").decode()
 
@@ -26,7 +28,9 @@ def numtostr(num, length):
 p = 46596819448382121528543993602166937859790681826228787290215278549306040186429954016289495405797367970633912738395422476766414007960033612236967483117581519241632909632385739856338602566387077652052439
 q = 8912415057377793721392143562801506106552469994282571523683575110438475980455175691024188269911341280568534105901762591911340612278434650445018659361480170403453079084543443195785479314147664610073737616917231
 
-def generate(p=p, q=q):
+
+def generate(p=number.getPrime(randint(660, 700)),
+             q=number.getPrime(randint(660, 700))):
     n = p*q
 
     phi = (p-1) * (q-1)
@@ -36,7 +40,7 @@ def generate(p=p, q=q):
         e = randint(2, carm_func)
         if np.gcd(e, carm_func) == 1: break
     d = int(modinv(e, phi))
-    return (n, e, d)
+    return (n, e, d, p, q)
 
 
 def encrypt(m, e, n):
@@ -51,7 +55,7 @@ def decrypt(c, d, n):
 
 
 
-if __name__ == "__main__":		
+if __name__ == "__main__":
     while True:
         msg = input("msg: ")
 
